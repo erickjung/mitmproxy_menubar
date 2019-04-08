c37adf5330f0c8def58fcff1d4ac385425bdef6b
Erick Jung <erick.jung@farfetch.com>
2019-04-08 14:21:36 +0100

    first version

---
+++ mitmproxy_menubar/scripts/ip_info.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+activeNetwork=$(route get default | grep interface | awk '{print $2}')
+activeNetworkName=$(networksetup -listallhardwareports | grep -B 1 "$activeNetwork" | awk '/Hardware Port/{ print }'|cut -d " " -f3-)
+networksetup -getinfo "$activeNetworkName"
